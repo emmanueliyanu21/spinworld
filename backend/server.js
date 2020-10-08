ba48b885cdddeda32f8b86ed7bdda0b382d6ee45
Emmanuel Iyanu <emmanueliyanu2012@gmail.com>
2020-10-08 16:07:33 +0100

    environment vairiable

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
 
 dotenv.config();
 
@@ -19,6 +19,9 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);

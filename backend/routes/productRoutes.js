b9d53a99000e11a5a91b77648fecf03c8c3d88ed
Emmanuel Iyanu <emmanueliyanu2012@gmail.com>
2020-10-09 02:34:20 +0100

    custom error handling

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,22 +7,29 @@ import Product from "../models/productModel.js";
 // @route GET /api/products
 // @access Fetch all products
 
-router.get("/", asyncHandler(async(req, res) => {
-  const products = await Product.find({});
-  res.json(products);
-}));
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
 
 // @desc Fetch single products
 // @route GET /api/products
 // @access pbulic
 
-router.get("/:id", asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
     if (product) {
-        res.json(product)
+      res.json(product);
     } else {
-        res.status(404).json({ message: 'Product not found ' });
+      res.status(404);
+      throw new Error("Product not found");
     }
-})); 
+  })
+);
 
 export default router;
--- backend/seeder.js
+++ backend/seeder.js
@@ -3,11 +3,10 @@ import dotenv from "dotenv";
 import colors from "colors";
 import users from "./data/users.js";
 import products from "./data/products.js";
-import Users from "./models/userModel.js";
+import User from "./models/userModel.js";
 import Product from "./models/productModel.js";
 import Order from "./models/orderModel.js";
 import connectDB from "./config/db.js";
-import User from "./models/userModel.js";
 
 dotenv.config();
 
@@ -21,15 +20,16 @@ const importData = async () => {
 
     const createdUsers = await User.insertMany(users);
 
-    const adminUsers = createdUsers[0]._id;
+    const adminUser = createdUsers[0]._id;
 
     const sampleProducts = products.map((product) => {
-      return { ...product, user: adminUsers };
+      return { ...product, user: adminUser };
     });
 
     await Product.insertMany(sampleProducts);
 
     console.log("Data Imported!".green.inverse);
+    process.exit();
   } catch (error) {
     console.error(`${error}`.red.inverse);
     process.exit(1);
@@ -49,8 +49,8 @@ const destroyData = async () => {
   }
 };
 
-if (process.argv[2] === '-d') {
-    destroyData()
+if (process.argv[2] === "-d") {
+  destroyData();
 } else {
-    importData()
+  importData();
 }

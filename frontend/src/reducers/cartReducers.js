b7d7f60dcc23e5b11f70f54265626be38012b3af
Emmanuel Iyanu <emmanueliyanu2012@gmail.com>
2020-10-12 10:57:08 +0100

    cart reducer and add to cart action

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,8 @@
+import { CART_ADD_ITEM } from "../constants/cartConstants";
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};

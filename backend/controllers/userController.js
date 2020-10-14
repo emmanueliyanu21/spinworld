34031169cf4a03a7d53de5d2c6dd772b766cf128
Emmanuel Iyanu <emmanueliyanu2012@gmail.com>
2020-10-14 05:14:43 +0100

    authentication middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
-import generateToken from '../utils/generateToken.js'
+import generateToken from "../utils/generateToken.js";
 
 // @desc Auth user and get token
 // @route POST /api/users/login
@@ -24,4 +24,23 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+// @desc Get user profile
+// @route GET /api/users/login
+// @access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("Invalid email or password");
+  }
+});
+
+export { authUser, getUserProfile };

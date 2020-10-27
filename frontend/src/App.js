d5e92275fb33d760d0e042382e0430c6e93d2109
Emmanuel Iyanu <emmanueliyanu2012@gmail.com>
2020-10-27 13:52:18 +0100

    creating user register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartSreen";
 import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Route path="/" component={HomeScreen} exact />
       <Route path="/product/:id" component={ProductScreen} />
       <Route path="/login" component={LoginScreen} />
+      <Route path="/register" component={RegisterScreen} />
       <Route path="/cart/:id?" component={CartScreen} />
       <Footer />
     </Router>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,13 +6,14 @@ import {
   productDetailsReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
-import { userLoginReducer } from "./reducers/userReducers";
+import { userLoginReducer, userRegisterReducer } from "./reducers/userReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
@@ -25,7 +26,7 @@ const userInfoFromStorage = localStorage.getItem("userInfo")
 
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
-  userLogin: { userInfo: userInfoFromStorage}
+  userLogin: { userInfo: userInfoFromStorage },
 };
 
 const middleware = [thunk];

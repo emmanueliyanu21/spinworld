a337c463228e2ca73c42ca4772c7f050156d306e
Emmanuel Iyanu <emmanueliyanu2012@gmail.com>
2020-10-27 14:18:31 +0100

    update profile endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,11 @@
 import express from "express";
 const router = express.Router();
-import { authUser, getUserProfile, registerUser } from "../controllers/userController.js";
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+} from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 // @desc Fetch all products
@@ -9,6 +14,9 @@ import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/").post(registerUser);
 router.post("/login", authUser);
-router.route("/profile").get(protect, getUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;

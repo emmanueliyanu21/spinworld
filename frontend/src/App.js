595b1a15e9ff6dec51e6c57d2a73b23033f7028b
Emmanuel Iyanu <emmanueliyanu2012@gmail.com>
2020-10-28 01:26:31 +0100

    payment screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
       <Route path="/login" component={LoginScreen} />
       <Route path="/profile" component={ProfileScreen} />
       <Route path="/register" component={RegisterScreen} />
+      <Route path="/payment" component={PaymentScreen} />
       <Route path="/shipping" component={ShippingScreen} />
       <Route path="/cart/:id?" component={CartScreen} />
       <Footer />
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -2,9 +2,13 @@ import {
   CART_ADD_ITEM,
   CART_REMOVE_ITEM,
   CART_SAVE_SHIPPING_ADDRESS,
+  CART_SAVE_PAYMENT_METHOD,
 } from "../constants/cartConstants";
 
-export const cartReducer = (state = { cartItems: [], shippingAddress: {}, },  action) => {
+export const cartReducer = (
+  state = { cartItems: [], shippingAddress: {} },
+  action
+) => {
   switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.payload;
@@ -33,6 +37,11 @@ export const cartReducer = (state = { cartItems: [], shippingAddress: {}, },  ac
       return {
         ...state,
         shippingAddress: action.payload,
+      };
+    case CART_SAVE_PAYMENT_METHOD:
+      return {
+        ...state,
+        PaymentMethod: action.payload,
       };
     default:
       return state;

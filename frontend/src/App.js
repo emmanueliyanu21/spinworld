2b5375b306d219b824fd56cb6457d06093e7502e
Emmanuel Iyanu <emmanueliyanu2012@gmail.com>
2020-10-29 10:10:09 +0100

    check for order

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import ProfileScreen from "./screens/ProfileScreen";
 import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderScreen";
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
       <Route path="/login" component={LoginScreen} />
       <Route path="/profile" component={ProfileScreen} />
       <Route path="/placeorder" component={PlaceOrderScreen} />
+      <Route path="/order/:id" component={OrderScreen} />
       <Route path="/register" component={RegisterScreen} />
       <Route path="/payment" component={PaymentScreen} />
       <Route path="/shipping" component={ShippingScreen} />

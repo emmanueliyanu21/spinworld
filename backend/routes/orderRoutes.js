a5506934b110268ff179fba82f6b91caca9d3e60
Emmanuel Iyanu <emmanueliyanu2012@gmail.com>
2020-11-24 13:26:23 +0100

    few changes

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 
-import { addOrderItems, getOrderById } from "../controllers/orderController.js";
+import { addOrderItems, getOrderById, updateOrderToPaid } from "../controllers/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 // @desc Fetch all products
@@ -10,5 +10,6 @@ import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/").post(protect, addOrderItems);
 router.route("/:id").get(protect, getOrderById);
+router.route("/:id/pay").put(protect, updateOrderToPaid);
 
 export default router;

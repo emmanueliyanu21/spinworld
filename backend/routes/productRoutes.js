e66f581e88e0f3074f44820457cb5f37c3c8d515
Emmanuel Iyanu <emmanueliyanu2012@gmail.com>
2020-12-03 12:30:43 +0100

    creating product page

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,14 +3,22 @@ const router = express.Router();
 import {
   getProducts,
   getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
 } from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 // @desc Fetch all products
 // @route GET /api/products
 // @access Fetch all products
 
-router.route("/").get(getProducts);
-router.route("/:id").get(getProductById);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 // @desc Fetch single products
 // @route GET /api/products
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import UserListScreen from "./screens/UserListScreen";
 import UserEditScreen from "./screens/UserEditScreen";
-// import ProductListScreen from "./screens/ProductListScreen";
+import ProductListScreen from "./screens/ProductListScreen";
 // import ProductEditScreen from "./screens/ProductEditScreen";
 // import OrderListScreen from "./screens/OrderListScreen";
 
@@ -38,6 +38,7 @@ function App() {
       <Route path="/cart/:id?" component={CartScreen} />
       <Route path="/admin/userlist" component={UserListScreen} />
       <Route path="/admin/user/:id/edit" component={UserEditScreen} />
+      <Route path="/admin/productlist" component={ProductListScreen} exact />
       <Footer />
     </Router>
   );

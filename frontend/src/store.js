7f62c50ea82afcaa5c84071ade08e60ad1f2354b
Emmanuel Iyanu <emmanueliyanu2012@gmail.com>
2020-12-10 18:46:18 +0100

    working on product screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,8 +7,8 @@ import {
   productDeleteReducer,
   productCreateReducer,
   productUpdateReducer,
-  // productReviewCreateReducer,
-  // productTopRatedReducer,
+  productReviewCreateReducer,
+  productTopRatedReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
 import {
@@ -36,8 +36,8 @@ const reducer = combineReducers({
   productDelete: productDeleteReducer,
   productCreate: productCreateReducer,
   productUpdate: productUpdateReducer,
-  // productReviewCreate: productReviewCreateReducer,
-  // productTopRated: productTopRatedReducer,
+  productReviewCreate: productReviewCreateReducer,
+  productTopRated: productTopRatedReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
